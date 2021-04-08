e68d9200e694819beed3025c785ca713cdfd8d4e
Nguyen Ngoc Duy <duy.nguyenngoc@ncc.asia>
2021-04-08 03:45:02 -0700

    fix lint

--- src/@next/components/atoms/AddNewTile/stories.tsx
+++ src/@next/components/atoms/AddNewTile/stories.tsx
@@ -1,8 +1,9 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
-
 import { AddNewTile } from ".";
 
+
+// eslint-disable-next-line no-undef
 storiesOf("@components/atoms/AddNewTile", module)
   .addParameters({ component: AddNewTile })
   .add("default", () => <AddNewTile type="card" />);
