f3f589021b6bda6758907825fda6dce1e78f22df
Nguyen Ngoc Duy <duy.nguyenngoc@ncc.asia>
2021-04-08 03:52:59 -0700

    fix

--- src/@next/components/atoms/AddNewTile/stories.tsx
+++ src/@next/components/atoms/AddNewTile/stories.tsx
@@ -1,9 +1,9 @@
+/* eslint-disable no-undef */
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import { AddNewTile } from ".";
 
 
-// eslint-disable-next-line no-undef
 storiesOf("@components/atoms/AddNewTile", module)
   .addParameters({ component: AddNewTile })
   .add("default", () => <AddNewTile type="card" />);
