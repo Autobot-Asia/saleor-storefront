46dd4e4b165a324a97fd16e71ed9f18a99ca31ab
namnguyenvan2k <nam.nguyenvan.ncc@gmail.com>
2021-07-21 16:32:50 +0700

    update font

--- src/@next/components/atoms/RichTextEditorContent/styles.ts
+++ src/@next/components/atoms/RichTextEditorContent/styles.ts
@@ -3,5 +3,11 @@ import { styled } from "@styles";
 export const Content = styled.div`
   .ce-block__content {
     max-width: 100%;
+    .ce-paragraph.cdx-block {
+      font: normal normal normal 16px Arial;
+      letter-spacing: 0.06px;
+      color: #000000;
+      opacity: 1;
+    }
   }
 `;
