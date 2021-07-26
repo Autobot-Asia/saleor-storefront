2faba0024f76987ee593b67fec340545dc35b81c
dinhvando <dinhdoronglua@gmail.com>
2021-07-26 15:20:25 +0700

    Fix: Homepage> Product Detail> Description item:  Description item not the same with design

--- src/@next/components/atoms/RichTextEditorContent/styles.ts
+++ src/@next/components/atoms/RichTextEditorContent/styles.ts
@@ -1,13 +1,16 @@
 import { styled } from "@styles";
 
 export const Content = styled.div`
-  .ce-block__content {
-    max-width: 100%;
-    .ce-paragraph.cdx-block {
-      font: normal normal normal 16px Arial;
-      letter-spacing: 0.06px;
-      color: #000000;
-      opacity: 1;
+  .codex-editor__redactor {
+    padding-bottom: 0 !important;
+    .ce-block__content {
+      max-width: 100%;
+      .ce-paragraph.cdx-block {
+        font: normal normal normal 16px Arial;
+        letter-spacing: 0.06px;
+        color: #000000;
+        opacity: 1;
+      }
     }
   }
 `;
