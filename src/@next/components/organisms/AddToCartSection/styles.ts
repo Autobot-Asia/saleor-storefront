8a3adc5cb1fedda183681cbc13e935528463b174
dinhvando <dinhdoronglua@gmail.com>
2021-07-28 10:40:09 +0700

    Fix style button Buy and AddToCart

--- src/@next/components/organisms/AddToCartSection/styles.ts
+++ src/@next/components/organisms/AddToCartSection/styles.ts
@@ -56,7 +56,7 @@ export const BoxAddTrolley = styled.div<IBoxAddTrolley>`
   height: 100%;
   width: 228px;
   border: 1px solid;
-  border-color: ${props => (props.isDisabled ? "#7D7D7D" : `${mainColorPage}`)};
+  border-color: ${props => (props.isDisabled ? "#7A7A7A" : `${mainColorPage}`)};
   display: flex;
   justify-content: center;
   align-items: center;
@@ -64,16 +64,10 @@ export const BoxAddTrolley = styled.div<IBoxAddTrolley>`
   color: ${mainColorPage};
   font: normal normal normal 16px/12px Arial;
   letter-spacing: 0.32px;
-  background-color: ${props => (props.isDisabled ? "#7D7D7D" : `${white}`)};
+  background-color: ${white};
   cursor: ${props => (props.isDisabled ? "default" : "pointer")};
-  /* img {
-    filter: ${props =>
-    props.isDisabled
-      ? "invert(50%) sepia(4%) saturate(16%) hue-rotate(317deg) brightness(97%) contrast(88%)"
-      : ""};
-  } */
   span {
-    color: ${props => (props.isDisabled ? `${white}` : `${mainColorPage}`)};
+    color: ${props => (props.isDisabled ? `#7A7A7A` : `${mainColorPage}`)};
   }
 `;
 
