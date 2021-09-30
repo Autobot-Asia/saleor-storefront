102eaa521a20226dad3d9674b2536191ec8e15aa
Công Lê <chicongxq2610@gmail.com>
2021-09-30 17:49:09 +0700

    address shipping

--- src/@next/components/organisms/DeliveryAddress/DeliveryAddress.tsx
+++ src/@next/components/organisms/DeliveryAddress/DeliveryAddress.tsx
@@ -9,10 +9,17 @@ import * as S from "./styles";
 export const DeliveryAddress = () => {
   return (
     <S.Wrapper>
-      <S.Title>
-        <img src={deliveryAddress} alt="" />
-        <FormattedMessage {...checkoutMessages.deliveryAddress} />
-      </S.Title>
+      <S.LeftContent>
+        <S.Title>
+          <img src={deliveryAddress} alt="" />
+          <FormattedMessage {...checkoutMessages.deliveryAddress} />
+        </S.Title>
+        <S.Info>
+          <S.FullName>Nguyễn Nam </S.FullName>| 58 Tố Hữu, Mễ Trì, Nam Từ Liêm,
+          Hà Nội | 0123456789
+        </S.Info>
+      </S.LeftContent>
+      <S.Edit>Chỉnh sửa</S.Edit>
     </S.Wrapper>
   );
 };
--- src/@next/components/organisms/DeliveryAddress/styles.ts
+++ src/@next/components/organisms/DeliveryAddress/styles.ts
@@ -5,6 +5,8 @@ export const Wrapper = styled.div`
   background-color: ${props => props.theme.colors.white};
   box-shadow: 0px 0px 10px #00000029;
   padding: 10px 15px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const Title = styled.div`
@@ -17,4 +19,21 @@ export const Title = styled.div`
   img {
     margin-right: 10px;
   }
+`;
+
+export const LeftContent = styled.div`
+  display: flex;
+`;
+
+export const Info = styled.div`
+  margin-left: 79px;
+`;
+
+export const FullName = styled.span`
+  font-weight: bold;
+`;
+
+export const Edit = styled.a`
+  color: #ff651c;
+  text-decoration: underline;
 `;
