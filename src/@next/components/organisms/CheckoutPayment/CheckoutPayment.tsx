929fabe5a496050fb2b30f96b96f6b2df19ea0e3
Công Lê <chicongxq2610@gmail.com>
2021-09-30 13:36:04 +0700

    fix bug 20473

--- src/@next/components/organisms/CheckoutPayment/CheckoutPayment.tsx
+++ src/@next/components/organisms/CheckoutPayment/CheckoutPayment.tsx
@@ -1,11 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { FormattedMessage } from "react-intl";
+import React from "react";
 
-import { Checkbox } from "@components/atoms";
-import { checkoutMessages } from "@temp/intl";
-
-import { DiscountForm } from "../DiscountForm";
-import { IDiscountFormData } from "../DiscountForm/types";
 import * as S from "./styles";
 import { IProps } from "./types";
 
@@ -21,63 +15,35 @@ const CheckoutPayment: React.FC<IProps> = ({
   removeVoucherCode,
   submitUnchangedDiscount,
 }) => {
-  const [showPromoCodeForm, setShowPromoCodeForm] = useState(
-    !!promoCodeDiscount?.voucherCode
-  );
+  // const [showPromoCodeForm, setShowPromoCodeForm] = useState(
+  //   !!promoCodeDiscount?.voucherCode
+  // );
 
-  useEffect(() => {
-    const isVoucherCode = !!promoCodeDiscount?.voucherCode;
-    if (isVoucherCode) {
-      setShowPromoCodeForm(isVoucherCode);
-    }
-  }, [promoCodeDiscount?.voucherCode]);
+  // useEffect(() => {
+  //   const isVoucherCode = !!promoCodeDiscount?.voucherCode;
+  //   if (isVoucherCode) {
+  //     setShowPromoCodeForm(isVoucherCode);
+  //   }
+  // }, [promoCodeDiscount?.voucherCode]);
 
-  const handleChangeShowPromoCodeForm = () => {
-    setShowPromoCodeForm(!showPromoCodeForm);
-  };
+  // const handleChangeShowPromoCodeForm = () => {
+  //   setShowPromoCodeForm(!showPromoCodeForm);
+  // };
 
-  const handleSubmitPromoCode = (discountForm?: IDiscountFormData) => {
-    const newPromoCode = discountForm?.promoCode;
-    const savedPromoCode = promoCodeDiscount?.voucherCode;
+  // const handleSubmitPromoCode = (discountForm?: IDiscountFormData) => {
+  //   const newPromoCode = discountForm?.promoCode;
+  //   const savedPromoCode = promoCodeDiscount?.voucherCode;
 
-    if ((!newPromoCode || !showPromoCodeForm) && savedPromoCode) {
-      removeVoucherCode(savedPromoCode);
-    } else if (newPromoCode && newPromoCode !== savedPromoCode) {
-      addPromoCode(newPromoCode);
-    } else {
-      submitUnchangedDiscount();
-    }
-  };
+  //   if ((!newPromoCode || !showPromoCodeForm) && savedPromoCode) {
+  //     removeVoucherCode(savedPromoCode);
+  //   } else if (newPromoCode && newPromoCode !== savedPromoCode) {
+  //     addPromoCode(newPromoCode);
+  //   } else {
+  //     submitUnchangedDiscount();
+  //   }
+  // };
 
-  return (
-    <S.Wrapper>
-      <section>
-        <S.Title data-test="checkoutPageSubtitle">
-          <FormattedMessage {...checkoutMessages.paymentMethod} />
-        </S.Title>
-        <Checkbox
-          data-test="checkoutPaymentPromoCodeCheckbox"
-          name="payment-promo-code"
-          checked={showPromoCodeForm}
-          onChange={handleChangeShowPromoCodeForm}
-        >
-          <FormattedMessage defaultMessage="Do you have a gift card voucher or discount code?" />
-        </Checkbox>
-        {showPromoCodeForm && (
-          <S.DiscountField>
-            <DiscountForm
-              discount={{ promoCode: promoCodeDiscount?.voucherCode }}
-              formId={promoCodeDiscountFormId}
-              formRef={promoCodeDiscountFormRef}
-              handleSubmit={handleSubmitPromoCode}
-              errors={promoCodeErrors}
-            />
-          </S.DiscountField>
-        )}
-        <S.Divider />
-      </section>
-    </S.Wrapper>
-  );
+  return <S.Wrapper />;
 };
 
 export { CheckoutPayment };
